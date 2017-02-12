2978d19989f59245e5bc9840d4f8c65c3b793d1f
Lauren Siegel <lcsiege@gmail.com>
2017-02-12 03:34:16 -0500

    Can upload own pictures now

--- app/src/main/java/com/example/lsiegel/pearlhacks2/CharacterChoice.java
+++ app/src/main/java/com/example/lsiegel/pearlhacks2/CharacterChoice.java
@@ -1,13 +1,60 @@
 package com.example.lsiegel.pearlhacks2;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class CharacterChoice extends AppCompatActivity {
-
+    private int PICK_IMAGE_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_choice);
+    }
+
+    /**
+     * allows user to select photo from gallery
+     * @author http://codetheory.in/android-pick-select-image-from-gallery-with-intents/
+     * @param view
+     */
+    public void addImage(View view){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+
+    }
+
+    /**
+     * @author http://codetheory.in/android-pick-select-image-from-gallery-with-intents/
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
